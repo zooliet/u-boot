e34e125fcfe225d01266f611e9f8ae105163e016
Junhyun Shin <hl1sqi@gmail.com>
2012-12-05 08:11:31 +0900

    added some misc

--- arch/arm/lib/board.c
+++ arch/arm/lib/board.c
@@ -527,7 +527,7 @@ void board_init_r(gd_t *id, ulong dest_addr)
 
 	/* The Malloc area is immediately below the monitor copy in DRAM */
 	malloc_start = dest_addr - TOTAL_MALLOC_LEN;
-	mem_malloc_init (malloc_start, TOTAL_MALLOC_LEN);
+	mem_malloc_init(malloc_start, TOTAL_MALLOC_LEN);
 
 #ifdef CONFIG_ARCH_EARLY_INIT_R
 	arch_early_init_r();
